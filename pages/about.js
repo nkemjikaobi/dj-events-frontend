1bd343ed4229d872711a74c9c4215745d5d0ac63
Obi NkemjIka Derick <obinkemjikaderick@Administrators-MacBook-Pro-7.local>
2021-07-15 00:25:19 +0100

    Pages and Routing

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>This is the about page</h1>
+            <p>This is an app to find the latest DJ and other musical events.</p>
+            <p>Version: 1.0.0</p>
+        </div>
+    )
+}
+
+export default AboutPage
