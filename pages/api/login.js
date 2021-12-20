3fe61e308c16052f0e5e7bb3fb4afd0c8101fac5
Obi NkemjIka Derick <nkemjikaobi@gmail.com>
2021-12-20 13:23:00 +0100

    Store JWT In Server HttpOnly Cookie

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,4 @@
-//import cookie from 'cookie';
+import cookie from 'cookie';
 import { BASE_URL } from '@/config/index';
 
 export default async (req, res) => {
@@ -20,16 +20,16 @@ export default async (req, res) => {
 
 		if (strapiRes.ok) {
 			// Set Cookie
-			// res.setHeader(
-			// 	'Set-Cookie',
-			// 	cookie.serialize('token', data.jwt, {
-			// 		httpOnly: true,
-			// 		secure: process.env.NODE_ENV !== 'development',
-			// 		maxAge: 60 * 60 * 24 * 7, // 1 week
-			// 		sameSite: 'strict',
-			// 		path: '/',
-			// 	})
-			// );
+			res.setHeader(
+				'Set-Cookie',
+				cookie.serialize('token', data.jwt, {
+					httpOnly: true,
+					secure: process.env.NODE_ENV !== 'development',
+					maxAge: 60 * 60 * 24 * 7, // 1 week
+					sameSite: 'strict',
+					path: '/',
+				})
+			);
 
 			res.status(200).json({ user: data.user });
 		} else {
