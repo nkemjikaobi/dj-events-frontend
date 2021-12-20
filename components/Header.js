5a0faab2da85cbe661828399b579acc90b8c2e66
Obi NkemjIka Derick <nkemjikaobi@gmail.com>
2021-12-20 10:43:43 +0100

    Login & Register Routes

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import styles from '@/styles/Header.module.css';
 import Search from './Search';
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 
 const Header = () => {
 	return (
@@ -22,6 +23,14 @@ const Header = () => {
 					<li>
 						<Link href='/events/add'>
 							<a>Add Event</a>
+						</Link>
+					</li>
+					<li>
+						<Link href='/account/login'>
+							<a className='btn-secondary btn-icon'>
+								<FaSignInAlt />
+								Login
+							</a>
 						</Link>
 					</li>
 				</ul>
